d2a4e54cdaf482c01f570908513508cfa26ba07f
unknown <rinoally@gmail.com>
2023-08-29 16:11:25 +0300

    add background image

--- src/components/GallerySection/GallerySection.jsx
+++ src/components/GallerySection/GallerySection.jsx
@@ -1,41 +1,40 @@
-import React from 'react'
-import Heading from '../Heading/Heading'
-import GalleryCart from '../GalleryCart/GalleryCart'
-import travelcard1img from '../../assets/travelcard1img.jpg'
-import imgPeople from '../../assets/imgPeople.png'
+import React from "react";
+import Heading from "../Heading/Heading";
+import GalleryCart from "../GalleryCart/GalleryCart";
+import travelcard1img from "../../assets/travelcard1img.jpg";
+import imgPeople from "../../assets/imgPeople.png";
+import "./GallerySection.css";
 
 const gallery = [
   {
     image: travelcard1img,
-    transport: 'на автобусе',
+    transport: "на автобусе",
     people: 10,
     imgPeople: imgPeople,
-    city: 'Тбилиси',
-    month: 'апрель',
-    price: 83000
-  }
-]
+    city: "Тбилиси",
+    month: "апрель",
+    price: 83000,
+  },
+];
 
 const GallerySection = () => {
   return (
     <div>
-      <Heading 
-      level='h1' text='Галерея путешествий'/>
-
-    {gallery.map((card) => (
-      <GalleryCart 
-        image={card.image}
-        transport={card.transport}
-        people={card.people}
-        imgPeople={card.imgPeople}
-        city={card.city}
-        month={card.month}
-        price={card.price}
-      />
-    ))}
-      
+      <Heading level="h1" text="Галерея путешествий" />
+      <div className="background">
+        {gallery.map((card) => (
+          <GalleryCart
+            transport={card.transport}
+            people={card.people}
+            imgPeople={card.imgPeople}
+            city={card.city}
+            month={card.month}
+            price={card.price}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GallerySection
+export default GallerySection;
